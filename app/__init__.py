9cea7c611a123b48cec3c3888a1da00bdaccff42
justin-curtis-lu <justin.curtis.lu@gmail.com>
2021-05-14 15:50:13 -0700

    SQLalchemy Db + migrations
    - Set Flaskenv
    - include Db
    - User and Post model w/ relationship
    - Migrations for production and test Db changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 # Read in config values
 app.config.from_object(Config)
+# Read in Db
+db = SQLAlchemy(app)
+# Migration Engine
+migrate = Migrate(app, db)
 
-from app import routes
+from app import routes, models
+
--- config.py
+++ config.py
@@ -1,4 +1,10 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dn32918hdnewqQDQd2qdeqx'
+
+    # Database Config (SQLite for now -> Migrate to Server Db for deployment )
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
