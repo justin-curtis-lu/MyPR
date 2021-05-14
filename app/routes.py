0354147d0142244562ec616cb9987838c95b1c75
justin-curtis-lu <justin.curtis.lu@gmail.com>
2021-05-14 15:25:46 -0700

    Login Form
    - login html
    - wtf forms
    - add login to routes

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
+from flask import render_template, flash, redirect, url_for
 
+# Landing Page
 @app.route('/')
 @app.route('/index')
 def index():
@@ -16,3 +19,17 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+# Login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # get LoginForm
+    form = LoginForm()
+    # Form processing
+    if form.validate_on_submit():
+        # True -> flash message and return index
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    # False -> Reload login page
+    return render_template('login.html', title='Sign In', form=form)
