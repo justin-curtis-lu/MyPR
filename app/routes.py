4300c2bfc6148ff6806bfe096587aae82ced0bc5
justin-curtis-lu <justin.curtis.lu@gmail.com>
2021-05-14 15:07:36 -0700

    Basic templates
    - Template inheritance
    - Example Users
    - Minor functionality

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Justin'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Bench PR!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'Squat PR!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
