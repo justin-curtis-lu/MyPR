b71cc5cd1ad96f3e6ca747fc99996121c8f3cd1b
justin-curtis-lu <justin.curtis.lu@gmail.com>
2021-05-14 16:50:16 -0700

    multiple edits
    - add notes
    - add user page
    - add edit profile page
    - add post page
    - add into routes, use datetime import for getting time stamps
    - use gravatar for profile pic

--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 # Mixin class for user implementations (authenticated, etc.)
 from flask_login import UserMixin
 from app import login
+from hashlib import md5
 
 # User model
 class User(UserMixin, db.Model):
@@ -15,6 +16,8 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(128))
     # Connection from user model to post model
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -26,6 +29,11 @@ class User(UserMixin, db.Model):
     # User check password
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
 
 # Post model
 class Post(db.Model):
