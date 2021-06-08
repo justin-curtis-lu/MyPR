7f479d29845ec47f1e94a661d6d487e115c8e0ac
justin-curtis-lu <justin.curtis.lu@gmail.com>
2021-06-07 18:03:53 -0700

    Elastic Search working locally
    - still need style
    - check on deployment

--- app/main/routes.py
+++ app/main/routes.py
@@ -69,7 +69,7 @@ def search():
         if total > page * current_app.config['POSTS_PER_PAGE'] else None
     prev_url = url_for('main.search', q=g.search_form.q.data, page=page - 1) \
         if page > 1 else None
-    return render_template('search.html', title=_('Search'), posts=posts,
+    return render_template('search.html', title='Search', posts=posts,
                            next_url=next_url, prev_url=prev_url)
 
 @bp.before_app_request
